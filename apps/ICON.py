20b60f6d34f0fb6dee78d11693dbab14373f4b06
River-Zhang <3190100057@zju.edu.cn>
2024-03-06 10:04:51 +0800

    update test

--- apps/ICON.py
+++ apps/ICON.py
@@ -722,7 +722,7 @@ class ICON(pl.LightningModule):
             rot_num=3,
             split={
                 "cape-easy": (0, 50),
-                "cape-hard": (50, 100)
+                "cape-hard": (50, 150)
             },
         )
         self.logger.experiment.add_hparams(
